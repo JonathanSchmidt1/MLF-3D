7a3994fd24a436de7f447d38d6714b0701cdc83e
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2020-01-23 14:56:58 +0100

    further fixes to test_functional

--- test_Functional.py
+++ test_Functional.py
@@ -8,16 +8,16 @@ from functools import partial
 import numpy as np
 
 
-grid_size = 60
+grid_size = 90
 filter_size = 10
 
-density = np.load(open("~/fusessh/H2_density.npy","rb"),allow_pickle=True).reshape(191,191,191) #center of molecule [96][96][99]
-density = density[96-grid_size//2:96+grid_size//2][:,96-grid_size//2:96+grid_size//2][:,:,99-grid_size//2:99+grid_size//2]
+density = np.load(open("../H2_density.npy","rb"),allow_pickle=True).reshape(191,191,191) #center of molecule [96][96][99]
+density = torch.tensor(density[96-grid_size//2:96+grid_size//2][:,96-grid_size//2:96+grid_size//2][:,:,99-grid_size//2:99+grid_size//2]).view(1,1, grid_size, grid_size, grid_size).float()
 
 Rs_in = [(1, 0)]  # 1 scalar field
 Rs_out = [(1, 0)]  # 1 vector field
 radial_window = partial(kernel.gaussian_window_wrapper, mode='conservative', border_dist=0., sigma=.6)
-conv = SE3Convolution(Rs_in, Rs_out, size=size, radial_window=radial_window)
+conv = SE3Convolution(Rs_in, Rs_out, size=filter_size, radial_window=radial_window)
 
 with torch.autograd.profiler.profile(use_cuda=False) as prof:
     for i in range(10):
